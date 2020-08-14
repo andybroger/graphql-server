783786dea605e0dcd19de89f96f11a0eb7239ae4
andreasbroger <andy@broger.io>
2020-08-14 08:35:34 +0200

    add computed field sample

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { ObjectType, Field } from 'type-graphql';
+import { ObjectType, Field, Root } from 'type-graphql';
 import { Entity, Property, Unique } from '@mikro-orm/core';
 import { Base } from 'utils/entities/base.entity';
 import UserValidator from 'contracts/validators/user.validator';
@@ -28,6 +28,11 @@ export class User extends Base<User> {
   @Field({ nullable: true })
   @Property({ nullable: true })
   public born?: Date;
+
+  @Field()
+  public name(@Root() parent: User): string {
+    return `${parent.firstName} ${parent.lastName}`;
+  }
 
   constructor(body: UserValidator) {
     super(body);
